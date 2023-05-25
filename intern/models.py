34b5a53acd266d06bc2936c95c953a3b1877d41c
luljn <luljn@Luljn.myguest.virtualbox.org>
2023-05-25 13:00:19 +0100

    Creation du model utilisateur, ajout des templates et authentification des utilisateurs implementee

--- intern/models.py
+++ intern/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+
+class User(AbstractUser) :
+
+    ADMIN = 'Encadreur'
+    INTERN = 'Stagiaire'
+
+    ROLES_CHOICES = (
+        (ADMIN, 'Encadreur'),
+        (INTERN, 'Stagiaire'),
+    )
+
+    user_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant utilisateur')
+    role = models.CharField(max_length=30, choices=ROLES_CHOICES, verbose_name='role de l\'utilisateur')
+    photo = models.ImageField(verbose_name='photo de profil')
--- intern/views.py
+++ intern/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+
+@login_required
+def home(request) :
+
+    return render(request, 'intern/App/index.html')
+
+
+
+@login_required
+def project(request) :
+
+    return render(request, 'intern/App/pages/project.html')
+
+
+
+@login_required
+def tasklist(request) :
+
+    return render(request, 'intern/App/pages/taskboard.html')
+
+
+
+class ProfilView(LoginRequiredMixin ,View) :
+
+    template_name = 'intern/App/pages/profile.html'
+
+
+
+    def get(self, request) : 
+
+        return render(request, self.template_name)
+    
+    def post(self, request) : 
+
+        pass
+
+
+
+class DetailledTaskView(LoginRequiredMixin ,View) :
+
+    template_name = 'intern/App/pages/task_detailled.html'
+
+    def get(self, request) :
+
+        return render(request, self.template_name)
+    
+    def post(self, request) : 
+
+        pass
