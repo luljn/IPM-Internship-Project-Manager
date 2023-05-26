aae790529fd6f3e9b55b2583485ada280f10f942
luljn <mbohlulajonathan4@gmail.com>
2023-05-26 15:01:38 +0100

    Migrations effectuées avec success et enregistrement des modèles dans la partie administration

--- intern/admin.py
+++ intern/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from intern.models import *
 
 # Register your models here.
+
+
+
+admin.site.register(User)
+# admin.site.register(Intern)
+# admin.site.register(Admin)
+admin.site.register(Intership)
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Phase)
+admin.site.register(Document)
--- intern/models.py
+++ intern/models.py
@@ -21,122 +21,124 @@ class User(AbstractUser) :
     
     
     
-# class Intern(User) :
+class Intern(User) :
     
-#     role = User.INTERN
-#     is_superuser = False
-    
-    
-    
-# class Admin(User) :
-    
-#     role = User.ADMIN
-#     is_superuser = True
+    role = User.INTERN
     
     
     
-# class Intership(models.Model) : 
+class Admin(User) :
     
-#     EN_ATTENTE = 'En attente'
-#     EN_COURS = 'En cours'
-#     TERMINE = 'Termine'
-#     ANNULE = 'Annule'
-    
-#     STATUS_CHOICES = (
-#         (EN_ATTENTE, 'En attente'),
-#         (EN_COURS, 'En cours'),
-#         (TERMINE, 'Termine'),
-#         (ANNULE, 'Annule'),
-#     )
-    
-#     intership_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant stage')
-#     duration = models.DurationField(verbose_name="durée du stage")
-#     start_date = models.DateField(verbose_name="date de début")
-#     end_date = models.DateField(verbose_name="date de fin")
-#     status = models.CharField(max_length=30, choices=STATUS_CHOICES, verbose_name="statut")
-#     user = models.ForeignKey(Intern, on_delete=models.CASCADE)
+    role = User.ADMIN
+    #is_superuser = models.BooleanField(default=True)
     
     
     
-# class Project(models.Model) :
+class Intership(models.Model) : 
     
-#     EN_ATTENTE = 'En attente'
-#     EN_COURS = 'En cours'
-#     TERMINE = 'Termine'
-#     ANNULE = 'Annule'
+    EN_ATTENTE = 'En attente'
+    EN_COURS = 'En cours'
+    TERMINE = 'Termine'
+    ANNULE = 'Annule'
     
-#     STATUS_CHOICES = (
-#         (EN_ATTENTE, 'En attente'),
-#         (EN_COURS, 'En cours'),
-#         (TERMINE, 'Termine'),
-#         (ANNULE, 'Annule'),
-#     )
+    STATUS_CHOICES = (
+        (EN_ATTENTE, 'En attente'),
+        (EN_COURS, 'En cours'),
+        (TERMINE, 'Termine'),
+        (ANNULE, 'Annule'),
+    )
     
-#     project_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant projet')
-#     title = models.CharField(max_length=500, verbose_name="titre")
-#     description = models.CharField(max_length=5500, verbose_name="description")
-#     start_date = models.DateField(verbose_name="date de début")
-#     end_date = models.DateField(verbose_name="date de fin")
-#     status = models.CharField(max_length=30, choices=STATUS_CHOICES, verbose_name="statut")
-#     internship = models.ForeignKey(Intership, on_delete=models.CASCADE)
-    
-    
-    
-# class Task(models.Model) :
-    
-#     EN_ATTENTE = 'En attente'
-#     EN_COURS = 'En cours'
-#     TERMINE = 'Termine'
-#     ANNULE = 'Annule'
-    
-#     STATUS_CHOICES = (
-#         (EN_ATTENTE, 'En attente'),
-#         (EN_COURS, 'En cours'),
-#         (TERMINE, 'Termine'),
-#         (ANNULE, 'Annule'),
-#     )
-    
-#     task_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant tâche')
-#     title = models.CharField(max_length=500, verbose_name="titre")
-#     description = models.CharField(max_length=5500, verbose_name="description")
-#     start_date = models.DateField(verbose_name="date de début")
-#     end_date = models.DateField(verbose_name="date de fin")
-#     status = models.CharField(max_length=30, choices=STATUS_CHOICES, verbose_name="statut")
-#     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    intership_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant stage')
+    duration = models.DurationField(null=True, verbose_name="durée du stage")
+    start_date = models.DateField(verbose_name="date de début")
+    end_date = models.DateField(verbose_name="date de fin")
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, verbose_name="statut")
+    user = models.ForeignKey(Intern, on_delete=models.CASCADE)
     
     
     
-# class Phase(models.Model) :
+class Project(models.Model) :
     
-#     EN_ATTENTE = 'En attente'
-#     EN_COURS = 'En cours'
-#     TERMINE = 'Termine'
-#     ANNULE = 'Annule'
+    EN_ATTENTE = 'En attente'
+    EN_COURS = 'En cours'
+    TERMINE = 'Termine'
+    ANNULE = 'Annule'
     
-#     STATUS_CHOICES = (
-#         (EN_ATTENTE, 'En attente'),
-#         (EN_COURS, 'En cours'),
-#         (TERMINE, 'Termine'),
-#         (ANNULE, 'Annule'),
-#     )
+    STATUS_CHOICES = (
+        (EN_ATTENTE, 'En attente'),
+        (EN_COURS, 'En cours'),
+        (TERMINE, 'Termine'),
+        (ANNULE, 'Annule'),
+    )
     
-#     phase_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant phase')
-#     title = models.CharField(max_length=500, verbose_name="titre")
-#     description = models.CharField(max_length=5500, verbose_name="description")
-#     start_date = models.DateField(verbose_name="date de début")
-#     end_date = models.DateField(verbose_name="date de fin")
-#     status = models.CharField(max_length=30, choices=STATUS_CHOICES, verbose_name="statut")
-#     project = models.ForeignKey(Project, on_delete=models.CASCADE) 
+    project_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant projet')
+    title = models.CharField(max_length=500, verbose_name="titre")
+    description = models.CharField(max_length=5500, verbose_name="description")
+    duration = models.DurationField(null=True, verbose_name="durée du stage")
+    start_date = models.DateField(verbose_name="date de début")
+    end_date = models.DateField(verbose_name="date de fin")
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, verbose_name="statut")
+    internship = models.ForeignKey(Intership, on_delete=models.CASCADE)
     
     
     
-# class Document(models.Model) :
+class Task(models.Model) :
     
-#     document_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant document')
-#     title = models.CharField(max_length=500, verbose_name="titre")
-#     description = models.CharField(max_length=5500, verbose_name="description")
-#     is_public = models.BooleanField(verbose_name="Autorisation de partage")
-#     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-#     phase = models.ForeignKey(Phase, on_delete=models.CASCADE)
-#     task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    EN_ATTENTE = 'En attente'
+    EN_COURS = 'En cours'
+    TERMINE = 'Termine'
+    ANNULE = 'Annule'
+    
+    STATUS_CHOICES = (
+        (EN_ATTENTE, 'En attente'),
+        (EN_COURS, 'En cours'),
+        (TERMINE, 'Termine'),
+        (ANNULE, 'Annule'),
+    )
+    
+    task_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant tâche')
+    title = models.CharField(max_length=500, verbose_name="titre")
+    duration = models.DurationField(null=True, verbose_name="durée du stage")
+    description = models.CharField(max_length=5500, verbose_name="description")
+    start_date = models.DateField(verbose_name="date de début")
+    end_date = models.DateField(verbose_name="date de fin")
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, verbose_name="statut")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    
+    
+    
+class Phase(models.Model) :
+    
+    EN_ATTENTE = 'En attente'
+    EN_COURS = 'En cours'
+    TERMINE = 'Termine'
+    ANNULE = 'Annule'
+    
+    STATUS_CHOICES = (
+        (EN_ATTENTE, 'En attente'),
+        (EN_COURS, 'En cours'),
+        (TERMINE, 'Termine'),
+        (ANNULE, 'Annule'),
+    )
+    
+    phase_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant phase')
+    title = models.CharField(max_length=500, verbose_name="titre")
+    description = models.CharField(max_length=5500, verbose_name="description")
+    duration = models.DurationField(null=True, verbose_name="durée du stage")
+    start_date = models.DateField(verbose_name="date de début")
+    end_date = models.DateField(verbose_name="date de fin")
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, verbose_name="statut")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) 
+    
+    
+    
+class Document(models.Model) :
+    
+    document_id = models.IntegerField(primary_key=True, auto_created=True, db_column='id', verbose_name='identifiant document')
+    title = models.CharField(max_length=500, verbose_name="titre")
+    description = models.CharField(max_length=5500, verbose_name="description")
+    is_public = models.BooleanField(default=False, verbose_name="Autorisation de partage")
+    project = models.ForeignKey(Project, null=True, on_delete=models.SET_NULL)
+    phase = models.ForeignKey(Phase, null=True, on_delete=models.SET_NULL)
+    task = models.ForeignKey(Task, null=True, on_delete=models.SET_NULL)
     
